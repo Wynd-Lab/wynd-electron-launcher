2a99a438ddc693ccdbc44cf615b4da7326d28efc
Patrice PETIT <ppetit@wynd.eu>
2021-07-21 09:53:35 +0200

    fix/zoom: set app commandLine

--- src/main/index.js
+++ src/main/index.js
@@ -215,3 +215,6 @@ app.whenReady()
 app.on('activate', () => {
 	if (store.windows.container.current === null) createWindows()
 })
+
+app.commandLine.appendSwitch('high-dpi-support', 1)
+app.commandLine.appendSwitch('force-device-scale-factor', 1)
