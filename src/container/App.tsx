60af43c00a5c8e8015286ef64db833d9d920de99
dasilvaeric <edasilva@wynd.eu>
2024-02-19 12:10:59 +0100

    Update App.tsx "allow-popups allow-forms"

--- src/container/App.tsx
+++ src/container/App.tsx
@@ -271,7 +271,7 @@ const App: React.FunctionComponent<IAppProps> = (props) => {
 				</Drawer>
 			)}
 			{readyToDiplayApp && urlApp && conf?.view === 'webview' && <webview title="wyndpos" id="e-launcher-frame" className={wyndposFrameCN} src={urlApp as string} preload={window.__STATIC__}></webview>}
-			{readyToDiplayApp && urlApp && conf?.view === 'iframe' && <iframe sandbox="allow-same-origin allow-scripts" title="wyndpos" id="e-launcher-frame" className={wyndposFrameCN} src={urlApp as string} onLoad={onLoad}></iframe>}
+			{readyToDiplayApp && urlApp && conf?.view === 'iframe' && <iframe sandbox="allow-same-origin allow-scripts allow-popups allow-forms" title="wyndpos" id="e-launcher-frame" className={wyndposFrameCN} src={urlApp as string} onLoad={onLoad}></iframe>}
 
 			{conf && conf.wpt && conf.wpt.enable && conf.wpt.url.href && display.ready && display.switch === 'WPT' && <iframe className="frame" title="wyndpostools" id="wpt-frame" src={conf.wpt.url.href}></iframe>}
 			{conf && conf.wpt && conf.report && conf.report.enable && display.switch === 'REPORT' && <ReportComponent onCallback={props.onCallback} />}
